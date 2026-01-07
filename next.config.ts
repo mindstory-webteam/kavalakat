5a234d7e3a688b422e00959578c1f61072cadb56
jishnu-NKS-Tec <jishnunarayanan378@gmail.com>
2026-01-07 10:46:03 +0530

    changes for deyploys

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/kavalakat',
+  assetPrefix: '/kavalakat/',
+  images: {
+    unoptimized: true,
+  },
+  // Disable trailing slashes for better compatibility
+  trailingSlash: true,
 };
 
 export default nextConfig;
