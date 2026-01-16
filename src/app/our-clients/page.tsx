4701c383aa6dc6bbd44222eb88d752dbd35707b9
jishnu-NKS-Tec <jishnunarayanan378@gmail.com>
2026-01-16 11:43:06 +0530

    gallery and clints and others

--- src/app/our-clients/page.tsx
+++ src/app/our-clients/page.tsx
@@ -20,14 +20,14 @@ const page = () => {
                     <div className="row g-xl-4 g-lg-3 g-4">
                         <div className="col-lg-4 d-lg-block d-none wow animate fadeInLeft" data-wow-delay="200ms" data-wow-duration="1500ms">
                             <div className="our-client-img-area">
-                                <Image width={540} height={7750} src="/assets/img/innerpages/our-client-img.jpg" alt="" />
+                                <Image width={540} height={7750} src="/assets/new-images/clints/clint-b-1.jpeg" alt="" />
                             </div>
                         </div>
                         <div className="col-lg-8">
                             <div className="row g-xl-4 g-lg-3 g-4 gy-md-5">
                                 <div className="col-md-4 col-sm-6 wow animate fadeInDown" data-wow-delay="200ms" data-wow-duration="1500ms">
                                     <div className="client-card magnetic-item">
-                                        <Image width={120} height={50} src="/assets/img/innerpages/client-logo1.png" alt="" />
+                                        <Image width={120} height={50} src="/assets/new-images/clints/1 (1).jpg" alt="" />
                                         <span>www.qzency.com</span>
                                         <svg width={35} height={35} viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.173267 0H34.9999V6.51953L6.58414 34.9996L0 28.4801L19.4059 9.2646L0.173267 9.43616V0Z" />
@@ -37,7 +37,7 @@ const page = () => {
                                 </div>
                                 <div className="col-md-4 col-sm-6 wow animate fadeInDown" data-wow-delay="300ms" data-wow-duration="1500ms">
                                     <div className="client-card magnetic-item">
-                                        <Image width={120} height={50} src="/assets/img/innerpages/client-logo2.png" alt="" />
+                                        <Image width={120} height={50} src="/assets/new-images/clints/10.jpg" alt="" />
                                         <span>www.vernex.com</span>
                                         <svg width={35} height={35} viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.173267 0H34.9999V6.51953L6.58414 34.9996L0 28.4801L19.4059 9.2646L0.173267 9.43616V0Z" />
@@ -47,7 +47,7 @@ const page = () => {
                                 </div>
                                 <div className="col-md-4 col-sm-6 wow animate fadeInDown" data-wow-delay="400ms" data-wow-duration="1500ms">
                                     <div className="client-card magnetic-item">
-                                        <Image width={120} height={50} src="/assets/img/innerpages/client-logo3.png" alt="" />
+                                        <Image width={120} height={50} src="/assets/new-images/clints/11.jpg" alt="" />
                                         <span>www.physical.com</span>
                                         <svg width={35} height={35} viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.173267 0H34.9999V6.51953L6.58414 34.9996L0 28.4801L19.4059 9.2646L0.173267 9.43616V0Z" />
@@ -57,7 +57,7 @@ const page = () => {
                                 </div>
                                 <div className="col-md-4 col-sm-6 wow animate fadeInDown" data-wow-delay="500ms" data-wow-duration="1500ms">
                                     <div className="client-card magnetic-item">
-                                        <Image width={120} height={50} src="/assets/img/innerpages/client-logo4.png" alt="" />
+                                        <Image width={120} height={50} src="/assets/new-images/clints/12.jpg" alt="" />
                                         <span>www.qzency.com</span>
                                         <svg width={35} height={35} viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.173267 0H34.9999V6.51953L6.58414 34.9996L0 28.4801L19.4059 9.2646L0.173267 9.43616V0Z" />
@@ -67,7 +67,7 @@ const page = () => {
                                 </div>
                                 <div className="col-md-4 col-sm-6 wow animate fadeInDown" data-wow-delay="600ms" data-wow-duration="1500ms">
                                     <div className="client-card magnetic-item">
-                                        <Image width={120} height={50} src="/assets/img/innerpages/client-logo5.png" alt="" />
+                                        <Image width={120} height={50} src="/assets/new-images/clints/14.jpg" alt="" />
                                         <span>www.vernex.com</span>
                                         <svg width={35} height={35} viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.173267 0H34.9999V6.51953L6.58414 34.9996L0 28.4801L19.4059 9.2646L0.173267 9.43616V0Z" />
@@ -77,7 +77,7 @@ const page = () => {
                                 </div>
                                 <div className="col-md-4 col-sm-6 wow animate fadeInDown" data-wow-delay="700ms" data-wow-duration="1500ms">
                                     <div className="client-card magnetic-item">
-                                        <Image width={120} height={50} src="/assets/img/innerpages/client-logo6.png" alt="" />
+                                        <Image width={120} height={50} src="/assets/new-images/clints/15.jpg" alt="" />
                                         <span>www.physical.com</span>
                                         <svg width={35} height={35} viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.173267 0H34.9999V6.51953L6.58414 34.9996L0 28.4801L19.4059 9.2646L0.173267 9.43616V0Z" />
@@ -87,7 +87,7 @@ const page = () => {
                                 </div>
                                 <div className="col-md-4 col-sm-6 wow animate fadeInDown" data-wow-delay="800ms" data-wow-duration="1500ms">
                                     <div className="client-card magnetic-item">
-                                        <Image width={120} height={50} src="/assets/img/innerpages/client-logo7.png" alt="" />
+                                        <Image width={120} height={50} src="/assets/new-images/clints/16.jpg" alt="" />
                                         <span>www.qzency.com</span>
                                         <svg width={35} height={35} viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.173267 0H34.9999V6.51953L6.58414 34.9996L0 28.4801L19.4059 9.2646L0.173267 9.43616V0Z" />
@@ -97,7 +97,7 @@ const page = () => {
                                 </div>
                                 <div className="col-md-4 col-sm-6 wow animate fadeInDown" data-wow-delay="800ms" data-wow-duration="1500ms">
                                     <div className="client-card magnetic-item">
-                                        <Image width={120} height={50} src="/assets/img/innerpages/client-logo8.png" alt="" />
+                                        <Image width={120} height={50} src="/assets/new-images/clints/16.jpg" alt="" />
                                         <span>www.vernex.com</span>
                                         <svg width={35} height={35} viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.173267 0H34.9999V6.51953L6.58414 34.9996L0 28.4801L19.4059 9.2646L0.173267 9.43616V0Z" />
@@ -107,7 +107,7 @@ const page = () => {
                                 </div>
                                 <div className="col-md-4 col-sm-6 wow animate fadeInDown" data-wow-delay="700ms" data-wow-duration="1500ms">
                                     <div className="client-card magnetic-item">
-                                        <Image width={120} height={50} src="/assets/img/innerpages/client-logo9.png" alt="" />
+                                        <Image width={120} height={50} src="/assets/new-images/clints/17.jpg" alt="" />
                                         <span>www.physical.com</span>
                                         <svg width={35} height={35} viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.173267 0H34.9999V6.51953L6.58414 34.9996L0 28.4801L19.4059 9.2646L0.173267 9.43616V0Z" />
@@ -117,7 +117,7 @@ const page = () => {
                                 </div>
                                 <div className="col-md-4 col-sm-6 wow animate fadeInDown" data-wow-delay="600ms" data-wow-duration="1500ms">
                                     <div className="client-card magnetic-item">
-                                        <Image width={120} height={50} src="/assets/img/innerpages/client-logo10.png" alt="" />
+                                        <Image width={120} height={50} src="/assets/new-images/clints/18.jpg" alt="" />
                                         <span>www.qzency.com</span>
                                         <svg width={35} height={35} viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.173267 0H34.9999V6.51953L6.58414 34.9996L0 28.4801L19.4059 9.2646L0.173267 9.43616V0Z" />
@@ -127,7 +127,7 @@ const page = () => {
                                 </div>
                                 <div className="col-md-4 col-sm-6 wow animate fadeInDown" data-wow-delay="500ms" data-wow-duration="1500ms">
                                     <div className="client-card magnetic-item">
-                                        <Image width={120} height={50} src="/assets/img/innerpages/client-logo11.png" alt="" />
+                                        <Image width={120} height={50} src="/assets/new-images/clints/19.jpg" alt="" />
                                         <span>www.vernex.com</span>
                                         <svg width={35} height={35} viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.173267 0H34.9999V6.51953L6.58414 34.9996L0 28.4801L19.4059 9.2646L0.173267 9.43616V0Z" />
@@ -137,7 +137,7 @@ const page = () => {
                                 </div>
                                 <div className="col-md-4 col-sm-6 wow animate fadeInDown" data-wow-delay="400ms" data-wow-duration="1500ms">
                                     <div className="client-card magnetic-item">
-                                        <Image width={120} height={50} src="/assets/img/innerpages/client-logo12.png" alt="" />
+                                        <Image width={120} height={50} src="/assets/new-images/clints/2 (2).jpg" alt="" />
                                         <span>www.physical.com</span>
                                         <svg width={35} height={35} viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.173267 0H34.9999V6.51953L6.58414 34.9996L0 28.4801L19.4059 9.2646L0.173267 9.43616V0Z" />
@@ -147,7 +147,7 @@ const page = () => {
                                 </div>
                                 <div className="col-md-4 col-sm-6 wow animate fadeInDown" data-wow-delay="300ms" data-wow-duration="1500ms">
                                     <div className="client-card magnetic-item">
-                                        <Image width={120} height={50} src="/assets/img/innerpages/client-logo13.png" alt="" />
+                                        <Image width={120} height={50} src="/assets/new-images/clints/20.jpg" alt="" />
                                         <span>www.egenslab.com</span>
                                         <svg width={35} height={35} viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.173267 0H34.9999V6.51953L6.58414 34.9996L0 28.4801L19.4059 9.2646L0.173267 9.43616V0Z" />
@@ -157,7 +157,7 @@ const page = () => {
                                 </div>
                                 <div className="col-md-4 col-sm-6 wow animate fadeInDown" data-wow-delay="200ms" data-wow-duration="1500ms">
                                     <div className="client-card magnetic-item">
-                                        <Image width={120} height={50} src="/assets/img/innerpages/client-logo14.png" alt="" />
+                                        <Image width={120} height={50} src="/assets/new-images/clints/21.jpg" alt="" />
                                         <span>www.egenstheme.com</span>
                                         <svg width={35} height={35} viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.173267 0H34.9999V6.51953L6.58414 34.9996L0 28.4801L19.4059 9.2646L0.173267 9.43616V0Z" />
@@ -167,7 +167,7 @@ const page = () => {
                                 </div>
                                 <div className="col-md-4 col-sm-6 wow animate fadeInDown" data-wow-delay="200ms" data-wow-duration="1500ms">
                                     <div className="client-card magnetic-item">
-                                        <Image width={120} height={50} src="/assets/img/innerpages/client-logo15.png" alt="" />
+                                        <Image width={120} height={50} src="/assets/new-images/clints/3 (2).jpg" alt="" />
                                         <span>www.egenslab.com</span>
                                         <svg width={35} height={35} viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.173267 0H34.9999V6.51953L6.58414 34.9996L0 28.4801L19.4059 9.2646L0.173267 9.43616V0Z" />
@@ -177,7 +177,7 @@ const page = () => {
                                 </div>
                                 <div className="col-md-4 col-sm-6 wow animate fadeInDown" data-wow-delay="300ms" data-wow-duration="1500ms">
                                     <div className="client-card magnetic-item">
-                                        <Image width={120} height={50} src="/assets/img/innerpages/client-logo16.png" alt="" />
+                                        <Image width={120} height={50} src="/assets/new-images/clints/4 (1).jpg" alt="" />
                                         <span>www.egenslab.com</span>
                                         <svg width={35} height={35} viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.173267 0H34.9999V6.51953L6.58414 34.9996L0 28.4801L19.4059 9.2646L0.173267 9.43616V0Z" />
@@ -187,7 +187,7 @@ const page = () => {
                                 </div>
                                 <div className="col-md-4 col-sm-6 wow animate fadeInDown" data-wow-delay="400ms" data-wow-duration="1500ms">
                                     <div className="client-card magnetic-item">
-                                        <Image width={120} height={50} src="/assets/img/innerpages/client-logo17.png" alt="" />
+                                        <Image width={120} height={50} src="/assets/new-images/clints/5 (1).jpg" alt="" />
                                         <span>www.egenstheme.com</span>
                                         <svg width={35} height={35} viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.173267 0H34.9999V6.51953L6.58414 34.9996L0 28.4801L19.4059 9.2646L0.173267 9.43616V0Z" />
@@ -197,7 +197,7 @@ const page = () => {
                                 </div>
                                 <div className="col-md-4 col-sm-6 wow animate fadeInDown" data-wow-delay="500ms" data-wow-duration="1500ms">
                                     <div className="client-card magnetic-item">
-                                        <Image width={120} height={50} src="/assets/img/innerpages/client-logo18.png" alt="" />
+                                        <Image width={120} height={50} src="/assets/new-images/clints/6.jpg" alt="" />
                                         <span>www.egenslab.com</span>
                                         <svg width={35} height={35} viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.173267 0H34.9999V6.51953L6.58414 34.9996L0 28.4801L19.4059 9.2646L0.173267 9.43616V0Z" />
