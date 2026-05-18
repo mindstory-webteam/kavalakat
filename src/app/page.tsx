f5e069d58d3f8847f0a90693197a9abec037e226
jishnu-NKS-Tec <jishnunarayanan378@gmail.com>
2026-05-18 11:53:18 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
     <>
     <Header />
      
-    {/* <Banner />
-    <HomeAboutSection /> */}
+    <Banner />
+    <HomeAboutSection />
     <HomeServiceSection />
     <HomePageFeatureSection />
    
